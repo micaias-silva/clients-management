d9f147e9bcbab8653ebbcb8f61ce530f2a2ef4b6
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-05 13:20:56 -0300

    feat: adicionada importação do módulo de autenticação

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientsModule } from './contacts/clients/clients.module';
-import { ContactsModule } from './contacts/contacts.module';
 import { ClientsModule } from './clients/clients.module';
 import { ContactsModule } from './contacts/contacts.module';
-import { ClientsModule } from './clients/clients.module';
-import { ContactsModule } from './contacts/contacts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ClientsModule, ContactsModule],
+  imports: [
+    ClientsModule,
+    ContactsModule,
+    MongooseModule.forRoot('mongodb://localhost/clients'),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
