e418a1f81aed6d5224c26c5383753c546fa18389
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-05 13:38:57 -0300

    feat: configuração do módulo de contatos

--- backend/src/contacts/contacts.module.ts
+++ backend/src/contacts/contacts.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { ContactsController } from './contacts.controller';
+import { ClientsModule } from 'src/clients/clients.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Contact, ContactSchema } from './schemas/contact.schema';
+import { Client, ClientSchema } from 'src/clients/schemas/client.schema';
 
 @Module({
+  imports: [
+    ClientsModule,
+    MongooseModule.forFeature([
+      { name: Contact.name, schema: ContactSchema },
+      { name: Client.name, schema: ClientSchema },
+    ]),
+  ],
   controllers: [ContactsController],
-  providers: [ContactsService]
+  providers: [ContactsService],
 })
 export class ContactsModule {}
