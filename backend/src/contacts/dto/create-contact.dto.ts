af8755a7196256a555c8e51f95f57c00b04bc364
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-05 13:37:58 -0300

    feat: data transfer objects com validação para os contatos

--- backend/src/contacts/dto/create-contact.dto.ts
+++ backend/src/contacts/dto/create-contact.dto.ts
@@ -1,1 +1,16 @@
-export class CreateContactDto {}
+import { Type } from 'class-transformer';
+import { IsDefined, IsEmail, IsPhoneNumber, MaxLength } from 'class-validator';
+
+export class CreateContactDto {
+  @IsDefined()
+  @MaxLength(128)
+  name: string;
+
+  @IsEmail()
+  @IsDefined()
+  @Type(() => String)
+  email: string;
+
+  @IsPhoneNumber('BR')
+  phoneNumber: string;
+}
