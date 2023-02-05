3f1602b30d21ada2f01682fe4ffea0b245c20733
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-05 13:35:34 -0300

    feat: nova implementação do hash da senha do cliente / mudança de diretório

--- backend/src/clients/shared/clients.service.ts
+++ backend/src/clients/shared/clients.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { hash } from 'bcrypt';
 import { Model } from 'mongoose';
 import { CreateClientDto } from '../dto/create-client.dto';
 import { UpdateClientDto } from '../dto/update-client.dto';
@@ -8,15 +9,18 @@ import { Client, ClientDocument } from '../schemas/client.schema';
 @Injectable()
 export class ClientsService {
   constructor(
-    @InjectModel(Client.name) private clientModel: Model<ClientDocument>,
+    @InjectModel(Client.name)
+    private clientModel: Model<ClientDocument>,
   ) {}
 
   async create(clientData: CreateClientDto) {
+    const hashedPassword = await hash(clientData.password, 10);
+    clientData.password = hashedPassword;
     return await this.clientModel.create(clientData);
   }
 
   async findAll() {
-    return await this.clientModel.find();
+    return await this.clientModel.find().populate('contacts');
   }
 
   async findOne(id: string) {
@@ -29,14 +33,27 @@ export class ClientsService {
     return client;
   }
 
-  async update(id: string, clientUpdateData: UpdateClientDto) {
-    const client = await this.findOne(id);
+  async findOneByEmail(email: string) {
+    const client = await this.clientModel.findOne({ email });
 
-    await client.update(clientUpdateData);
+    if (!client) {
+      throw new NotFoundException();
+    }
+
+    return client;
+  }
+
+  async update(id: string, clientUpdateData: UpdateClientDto) {
+    if (clientUpdateData.password) {
+      const hashedPassword = await hash(clientUpdateData.password, 10);
+      clientUpdateData.password = hashedPassword;
+    }
+    await this.findOne(id);
+    await this.clientModel.updateOne({ _id: id }, clientUpdateData);
   }
 
   async remove(id: string) {
-    const client = await this.findOne(id);
-    await client.remove();
+    await this.findOne(id);
+    await this.clientModel.remove({ _id: id });
   }
 }
