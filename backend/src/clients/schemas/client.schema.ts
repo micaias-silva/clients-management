5687dada7d917b5defb1d498d6652d936180940f
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-05 13:19:47 -0300

    fix: configurada referência de relacionamento entre Clients e Contact

--- backend/src/clients/schemas/client.schema.ts
+++ backend/src/clients/schemas/client.schema.ts
@@ -6,17 +6,22 @@ export type ClientDocument = HydratedDocument<Client>;
 
 @Schema({ timestamps: true })
 export class Client {
-  @Prop()
+  @Prop({ required: true })
   name: string;
 
-  @Prop()
+  @Prop({ required: true })
   email: string;
 
-  @Prop()
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
   phoneNumber: string;
 
-  @Prop({ type: Types.ObjectId, ref: 'Contacts' })
-  contacts: Contact[];
+  @Prop({
+    type: [{ type: Types.ObjectId, ref: 'Contact' }],
+  })
+  contacts?: Contact[];
 }
 
 export const ClientSchema = SchemaFactory.createForClass(Client);
