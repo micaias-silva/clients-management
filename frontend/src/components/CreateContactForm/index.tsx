59dd610a481486df74d9347c65d516f82dc089c8
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-09 09:05:08 -0300

    feat: integração com o modal e contexto de atualização

--- frontend/src/components/CreateContactForm/index.tsx
+++ frontend/src/components/CreateContactForm/index.tsx
@@ -1,13 +1,20 @@
 import "./style.css";
-import { useContext } from "react";
 import * as yup from "yup";
+import backendApi from "../../services/backendApi";
+import { useContext } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
-import backendApi from "../../services/backendApi";
 import { TokenContext } from "../../providers/token";
+import { UpdateContext } from "../../providers/update";
 
-const CreateContactForm = () => {
-  const ctx = useContext(TokenContext);
+interface CreateContactProps {
+  closeModal: () => void;
+}
+
+const CreateContactForm = ({ closeModal }: CreateContactProps) => {
+  const tokenCtx = useContext(TokenContext);
+  const updateCtx = useContext(UpdateContext);
+
   const phoneRegex = /^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/;
 
   const contactSchema = yup.object().shape({
@@ -27,28 +34,38 @@ const CreateContactForm = () => {
     resolver: yupResolver(contactSchema),
   });
 
-  const onSubmitFunction = (data: any) => {
-    backendApi.post("/contacts", data, {
-      headers: { Authorization: ctx.token },
+  const createContact = async (data: any) => {
+    await backendApi.post("/contacts", data, {
+      headers: { Authorization: tokenCtx.token },
     });
+    updateCtx.refresh();
+  };
+
+  const onSubmitFunction = (data: any) => {
+    createContact(data);
+    closeModal();
   };
 
   return (
-    <>
+    <div>
       <div className="fill"></div>
       <form onSubmit={handleSubmit(onSubmitFunction)}>
-        <h2>Criar novo contato</h2>
+        <h2>Criar contato</h2>
+
         <span>{errors.name ? (errors.name?.message as string) : null}</span>
         <input {...register("name")} placeholder="Nome" />
+
         <span>{errors.email ? (errors.email?.message as string) : null}</span>
         <input {...register("email")} type="email" placeholder="E-mail" />
+
         <span>
           {errors.phoneNumber ? (errors.phoneNumber?.message as string) : null}
         </span>
         <input {...register("phoneNumber")} type="tel" placeholder="Telefone" />
+
         <button type="submit">Enviar</button>
       </form>
-    </>
+    </div>
   );
 };
 export default CreateContactForm;
