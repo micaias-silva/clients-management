eecbc786249606bcd691607b3e5bc859cb31e5b5
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-09 15:06:36 -0300

    feat: tratativa de erros do banco de dados

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MongooseError } from 'mongoose';
 import { AppModule } from './app.module';
+import { MongoExceptionFilter } from './filters/mongo-exception.filter';
+import { MongooseExceptionFilter } from './filters/mongoose-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.useGlobalFilters(
+    new MongooseExceptionFilter(),
+    new MongoExceptionFilter(),
+  );
   await app.listen(3000);
 }
 bootstrap();
