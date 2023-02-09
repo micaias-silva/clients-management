6e4a634c61f1ad18798ea13eb69577c078206263
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-09 12:40:18 -0300

    feat: adicionada biblioteca react-toastify

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,28 @@
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Providers from "./providers";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Providers>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
         <App />
       </Providers>
     </BrowserRouter>
