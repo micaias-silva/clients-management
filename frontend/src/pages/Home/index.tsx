0f62a8ac23de0f9ae939986921b1c2ffcda051cf
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-09 09:17:40 -0300

    feat: página inicial

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,43 @@
+import { useContext, useEffect, useState } from "react";
+import { TokenContext } from "../../providers/token";
+import { Redirect } from "react-router-dom";
+import backendApi from "../../services/backendApi";
+import ContactCard from "../../components/ContatcCard";
+import SearchBar from "../../components/SearchBar";
+import CreateConctactButton from "../../components/CreateContactButton";
+import { UpdateContext } from "../../providers/update";
+
+const HomePage = () => {
+  const tokenCtx = useContext(TokenContext);
+  const updateCtx = useContext(UpdateContext);
+
+  const [contactsList, setContactsList] = useState<any[]>([]);
+  const [showedList, setShowedList] = useState<any[]>([]);
+
+  useEffect(() => {
+    backendApi
+      .get("/contacts", { headers: { Authorization: tokenCtx.token } })
+      .then((res) => {
+        setContactsList(res.data);
+        setShowedList(res.data);
+      });
+  }, [updateCtx.update]);
+
+  return (
+    <div className="container">
+      {!tokenCtx.token ? (
+        <Redirect to="/login" />
+      ) : (
+        <section>
+          <CreateConctactButton />
+          <SearchBar baseArray={contactsList} setList={setShowedList} />
+          {showedList.map((contact) => (
+            <ContactCard key={contact.id} {...contact} />
+          ))}
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
